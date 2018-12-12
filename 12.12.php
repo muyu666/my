1f7e7200c58c8943a303ecdfe1411dc9213539d0
muyu666 <44764165+muyu666@users.noreply.github.com>
2018-12-12 08:36:40 +0800

    Add files via upload

---
+++ 12.12.php
@@ -0,0 +1,17 @@
+<?php 
+
+$num1 = 1;
+$num2 = 2;
+$info = my($num1,$num2);
+var_dump($info);
+
+function my($num1,$num2){
+
+	$num1 = $arr[];
+	$num2 = $arr[];
+
+	$res = array_sum($num1,$num2);
+	return $res;
+}
+
+?>
